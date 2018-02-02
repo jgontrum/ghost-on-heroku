08c27d29d24e010e24774094573df8e966f693aa
jgontrum <gontrum@me.com>
2018-02-02 14:02:39 +0100

    Use custom start script to overwrite installed_apps value in ghost database.

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: npm start --production
+web: bin/start
--- bin/common/env-values.js
+++ bin/common/env-values.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mysqlDatabaseUrl: process.env.MYSQL_DATABASE_URL || process.env.JAWSDB_URL || process.env.CLEARDB_DATABASE_URL
+  mysqlDatabaseUrl: process.env.MYSQL_DATABASE_URL || process.env.JAWSDB_URL || process.env.CLEARDB_DATABASE_URL,
+  activeApps: process.env.GHOST_APPS || ""
 }
